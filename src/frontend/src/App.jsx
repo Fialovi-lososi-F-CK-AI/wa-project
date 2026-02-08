c5c7ea6b7c1d143c84b1d6930380a7e243652a01
icykcypher <solonitsyn.maksym@gmail.com>
2026-02-08 01:34:32 +0100

    trying nginx zero downtime reload #2

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Deployed via GitHub Actions CD with Ansible</h1>
+      <h1>Deployed via GitHub Actions CD with Ansible #2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
